f13b5e24632b786fffbc9d1717cd2df96c5f2c42
Matt <matthew@amitheous.com>
2023-12-01 23:06:01 -0700

    Fix: Expense History View - Pagination for smooth scrolling - Render items into flatlist

--- app/(tabs)/home/ExpenseHistoryModal.js
+++ app/(tabs)/home/ExpenseHistoryModal.js
@@ -1,57 +1,87 @@
-import { View, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, StyleSheet, FlatList } from "react-native";
 import { Link, useNavigation } from "expo-router";
-import { Appbar, Card, Text, Button, useTheme } from "react-native-paper";
+import { Appbar, Card, Text, Button, useTheme, Divider } from "react-native-paper";
 import { AuthStore } from "../../../store";
 import { useStoreState } from "pullstate";
 
-
 export default function Modal() {
-    const expenses = useStoreState(AuthStore, s => s.expenses);
-    const categories = useStoreState(AuthStore, s => s.categories);
-    const navigation = useNavigation();
-    const theme = useTheme();
+  const expenses = useStoreState(AuthStore, (s) => s.expenses);
+  const categories = useStoreState(AuthStore, (s) => s.categories);
+  const navigation = useNavigation();
+  const theme = useTheme();
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
 
-    const styles = StyleSheet.create({
-        header: {
-          backgroundColor: theme.colors.secondaryContainer
-        },
-        headerContent: {
-          color: theme.colors.secondary
-        },
-        card: {
-          backgroundColor: theme.colors.tertiaryContainer,
-          fontFamily: 'Montserrat_400Regular',
-          marginVertical: 5
-        },
-        cardText: {
-          color: theme.colors.onTertiaryContainer,
-          fontFamily: 'Montserrat_400Regular',
-        }
-      });
+  const styles = StyleSheet.create({
+    header: {
+      backgroundColor: theme.colors.secondaryContainer,
+    },
+    headerContent: {
+      color: theme.colors.secondary,
+    },
+    card: {
+      backgroundColor: theme.colors.tertiaryContainer,
+      fontFamily: "Montserrat_400Regular",
+      marginVertical: 1,
+    },
+    cardText: {
+      color: theme.colors.onTertiaryContainer,
+      fontFamily: "Montserrat_400Regular",
+    },
+  });
 
-      const fetchCategory = (categoryId) => {
-        const category = categories.find(cat => cat.id === categoryId);
-        return category ? category.name : 'No Category'
-      }
-    
+  const fetchCategory = (categoryId) => {
+    const category = categories.find((cat) => cat.id === categoryId);
+    return category ? category.name : "No Category";
+  };
+
+  const loadMoreData = () => {
+    const newData = expenses.slice(0, page * 40);
+    setData(newData);
+    setPage(page + 1);
+  };
+
+  useEffect(() => {
+    loadMoreData();
+  }, []);
+
+  const renderExpenseItem = ({ item }) => (
+    <Card style={styles.card}>
+      <Card.Title
+        titleVariant="titleMedium"
+        title={item.title}
+        titleStyle={styles.cardText}
+      />
+      <Card.Content>
+        <Text style={styles.cardText}>
+          Amount: ${item.amount.toLocaleString("en-US", {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+          })}
+        </Text>
+        <Text style={styles.cardText}>
+          Category: {fetchCategory(item?.category)}
+        </Text>
+      </Card.Content>
+    </Card>
+  );
 
   return (
     <View style={{ flex: 1, backgroundColor: theme.colors.background }}>
-        <Appbar.Header style={styles.header} >
-            <Appbar.BackAction onPress={() => navigation.goBack()} />
-            <Appbar.Content title="Expense History"/> 
-        </Appbar.Header>
-        <View>
-        {expenses.map((expense, index) => (
-            <Card key={index} style={styles.card}>
-            <Card.Title titleVariant="titleMedium" title={expense.title} titleStyle={styles.cardText} />
-            <Card.Content>
-                <Text style={styles.cardText}>Amount: ${expense.amount.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</Text>
-                <Text style={styles.cardText}>Category:  {fetchCategory(expense?.category)}</Text>
-            </Card.Content>
-            </Card>
-        ))}
-        </View>
+      <Appbar.Header style={styles.header}>
+        <Appbar.BackAction onPress={() => navigation.goBack()} />
+        <Appbar.Content title="Expense History" />
+      </Appbar.Header>
+      <Divider style={{marginVertical: 1, height: 0}} />
+      <FlatList
+        data={data}
+        renderItem={renderExpenseItem}
+        keyExtractor={(item, index) => index.toString()}
+        onEndReached={loadMoreData}
+        onEndReachedThreshold={0.5}
+      />
     </View>
   );
 }
