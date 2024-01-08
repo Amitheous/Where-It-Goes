1d60d0d1b3f0be0691b6c9a7bb576ab5475b65bb
Matt <matthew@amitheous.com>
2024-01-07 22:33:45 -0700

    Implement: react-native-dotenv - Babel config - importing firebase key values - removing dotenv

--- firebaseKeys.js
+++ firebaseKeys.js
@@ -1,12 +1,19 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import {
+    FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN,
+    FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET,
+    FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID,
+    FIREBASE_MEASUREMENT_ID
+} from '@env';
 
 export const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_AUTH_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_AUTH_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_AUTH_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_AUTH_APP_ID,
-    measurementId: process.env.FIREBASE_AUTH_MEASUREMENT_ID
+    apiKey: FIREBASE_API_KEY,
+    authDomain: FIREBASE_AUTH_DOMAIN,
+    projectId: FIREBASE_PROJECT_ID,
+    storageBucket: FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+    appId: FIREBASE_APP_ID,
+    measurementId: FIREBASE_MEASUREMENT_ID
 };
