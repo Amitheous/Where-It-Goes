7a86cdcb22dc356b9fd6cfcf82a488f103e26569
Matt <matthew@amitheous.com>
2024-01-09 21:23:58 -0700

    Updated Metro Config

--- metro.config.js
+++ metro.config.js
@@ -2,11 +2,4 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-config.transformer.getTransformOptions = async () => ({
-    transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-    },
-});
-
 module.exports = config;
